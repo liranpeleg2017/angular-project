1a387153bd20218fc89f17798146260a000e8eb9
Liran Peleg <pharoah29@gmail.com>
2017-07-10 19:51:16 +0300

    'branch....'

--- src/app/app.router.module.ts
+++ src/app/app.router.module.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // import { ModuleWithProviders  } from "@angular/core";
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -81,3 +82,88 @@ export const RoutingComponents =
 ];
 
 
+=======
+// import { ModuleWithProviders  } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AboutComponent } from './components/about/about.component'
+import { HelpComponent } from './components/help/help.component'
+import { HomeComponent } from './components/home/home.component'
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component'
+import { MovieComponent } from "./components/movie/movie.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { SignUpComponent } from "./components/signup/signup.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RoutingAccessGuard } from "app/routing.guard";
+
+ 
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    pathMatch: 'full',
+  
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent,
+    
+  },
+
+  {
+    path: 'home',
+    component:HomeComponent,
+    canActivate: [RoutingAccessGuard]
+  },  
+
+  {
+    path: 'about',
+    component: AboutComponent
+  }, 
+ 
+  
+
+  {
+      path: 'help',
+      component:HelpComponent
+  },
+
+   {
+      path: 'movies',
+      component:MoviesListComponent
+  },
+
+   {
+      path: '**',
+      component:PageNotFoundComponent
+  }  
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes/* ,{ useHash: true } ng build --prod*/)],
+  exports: [RouterModule],
+  providers: [RoutingAccessGuard]
+})
+export class AppRouterModule 
+{ 
+
+}
+
+export const RoutingComponents = 
+[
+            AboutComponent,
+            HelpComponent,
+            HomeComponent,
+            PageNotFoundComponent,
+            HeaderComponent,
+            SignUpComponent,
+            LoginComponent
+];
+
+
+>>>>>>> b2c85472d9c558fd226f6516022e8ad0ca9113a0
